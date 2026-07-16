15faed7a6c47769431eba1ca4106207dc9563ac5
Siddesh Rizwani <siddeshrizwani@gmail.com>
2026-07-16 13:24:49 +0530

    Implement database schemas

--- src/db/db.js
+++ src/db/db.js
@@ -12,4 +12,8 @@ export const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
 
+pool.on('error', (err) => {
+    console.error('Unexpected error on idle PostgreSQL client', err);
+});
+
 export const db = drizzle(pool);
